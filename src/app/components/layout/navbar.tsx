45c9c0a119705a6815fbdbbafd7729a96073597e
ashim <magarashim69086@gmail.com>
2025-06-17 21:19:33 +0545

    mobile responsive

--- src/app/components/layout/navbar.tsx
+++ src/app/components/layout/navbar.tsx
@@ -12,7 +12,7 @@ function Navbar() {
     ];
 
     return (
-        <nav className="sticky top-0 z-50 bg-white font-sans px-4 md:px-8 py-4 rounded-4xl">
+        <nav className="sticky top-0 z-50 bg-white font-sans px-4  py-4 rounded-4xl">
             <div className="flex justify-between items-center">
                 <h1 className="font-bold text-lg pt-3">Portfolio</h1>
 
