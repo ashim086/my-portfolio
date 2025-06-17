576f3e7c3a4dcce28bd54ff0ce7b05e45b5dd8dd
ashim <magarashim69086@gmail.com>
2025-06-17 21:41:14 +0545

    mobile responsive

--- src/app/components/home/projects.tsx
+++ src/app/components/home/projects.tsx
@@ -17,7 +17,7 @@ function Project() {
 
     return (
 
-        <main >
+        <main className='md:pt-26'>
 
             <Title title='Projects' subtitle='Recent Projects' />
             <div className='grid md:grid-cols-1 lg:grid-cols-2 space-x-9 space-y-9 mx-3 w-full'>
