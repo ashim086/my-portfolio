0a265747fb37f788d05943ca661cfa3e83d6fcdd
ashim <magarashim69086@gmail.com>
2026-03-11 15:40:44 +0545

    optimized seo

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,6 +1,6 @@
 export default function Loading() {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-white/85">
+    <div className="flex items-center justify-center min-h-screen bg-white">
       <video
         autoPlay
         loop
