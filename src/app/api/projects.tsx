9044d969f713a22026655af3510aa80257b9b297
ashim <magarashim69086@gmail.com>
2026-03-14 01:35:27 +0545

    cv update d

--- src/app/api/projects.tsx
+++ src/app/api/projects.tsx
@@ -1,5 +1,17 @@
 export const projectItems = [
     {
+        name: 'Payment Integration Demo',
+        photo: '/payment.png',
+        description: 'A demo project showcasing secure online payment integration and transaction handling.',
+        link: 'https://wallets-sooty.vercel.app/login'
+    },
+    {
+        name: 'Relocation Booking Platform',
+        photo: '/relocation.png',
+        description: 'A full-stack platform for booking and managing home or office relocation services with role-based dashboards.',
+        link: 'https://texch.vercel.app/'
+    },
+    {
         name: 'Medi-care',
         photo: '/medi.png',
         description: 'An ecommerce platform for buying proteins and vitamin',
